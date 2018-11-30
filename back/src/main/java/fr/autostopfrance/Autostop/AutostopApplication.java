fb7c05a69cfdc7135501a546681333c14d88299b
Zakaria Hamichi <Hamichi.zakaria@gmail.com>
2018-11-30 16:47:38 +0100

    Debug git

---
+++ back/src/main/java/fr/autostopfrance/Autostop/AutostopApplication.java
@@ -0,0 +1,12 @@
+package fr.autostopfrance.Autostop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutostopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutostopApplication.class, args);
+	}
+}
