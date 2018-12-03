2c14aecc2abf111edb53550fc02ec50f288a8ffa
Alexandre GUERIN <alexandre.guerin17@gmail.com>
2018-12-03 09:03:32 +0100

    radius bar added in profile menu

--- Front/autostop/src/pages/home/connexion/main/menu/tabs/profile/distance/distance.ts
+++ Front/autostop/src/pages/home/connexion/main/menu/tabs/profile/distance/distance.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the DistancePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @Component({
   selector: 'page-distance',
   templateUrl: 'distance.html',
 })
 export class DistancePage {
+
+  radius : number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
