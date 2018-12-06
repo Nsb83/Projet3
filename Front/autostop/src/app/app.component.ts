290be82295decf12f467cf4b56a6b39f2af14297
Nicolas Barbier <Nicolas-Barbier_student2018@wilder.school>
2018-12-06 13:30:30 +0100

    Splash screen

--- Front/autostop/src/app/app.component.ts
+++ Front/autostop/src/app/app.component.ts
@@ -17,7 +17,7 @@ export class MyApp {
     splashScreen: SplashScreen
   ) {
     platform.ready().then(() => {
-      this.rootPage = MainPage;
+      this.rootPage = HomePage;
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
