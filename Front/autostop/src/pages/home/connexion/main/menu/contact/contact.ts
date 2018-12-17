76051521977f16ab5a4b94db479122c4d51f3faa
JN Bourrat <jn.brrt@yahoo.fr>
2018-12-17 16:20:35 +0100

    Made a button to open in-device mail app

--- Front/autostop/src/pages/home/connexion/main/menu/contact/contact.ts
+++ Front/autostop/src/pages/home/connexion/main/menu/contact/contact.ts
@@ -1,12 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { EmailComposer } from '@ionic-native/email-composer';
+import { User } from '../../../../../../models/User';
 
-/**
- * Generated class for the ContactPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @Component({
   selector: 'page-contact',
@@ -14,11 +10,41 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild('message') message;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private emailComposer: EmailComposer) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ContactPage');
   }
+
+// ****************************************************************
+//        METHOD TO BE COMPLETED WITH USER'S EMAIL ADRESS
+//          & TESTED ON DEVICE WITH MAIL APP INSTALLED
+// ****************************************************************
+  onSubmit(){
+    this.emailComposer.isAvailable().then((available: boolean) =>{
+      if(available) {
+        //Now we know we can send
+      }
+     });
+     
+     let email = {
+       to: 'wildautostop@gmail.com',
+       cc: '',
+       bcc: ['', ''],
+       attachments: [
+       ],
+       subject: 'Message from autostop user ', // TO BE COMPLETED WITH USER'S NAME
+       body: this.message.value,
+       isHtml: true
+     };
+     
+     // Send a text message using default options
+     this.emailComposer.open(email);
+     console.log(email);
+  }
+// ****************************************************************
 
 }
