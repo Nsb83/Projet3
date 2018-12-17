72857a0f21f301cff2088c85a9d59074759c5862
Nicolas Barbier <Nicolas-Barbier_student2018@wilder.school>
2018-12-17 17:13:39 +0100

    Page Profil et nom dans menu

--- Front/autostop/src/pages/home/connexion/main/menu/tabs/profile/profile.ts
+++ Front/autostop/src/pages/home/connexion/main/menu/tabs/profile/profile.ts
@@ -1,21 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { UserProvider } from '../../../../../../../providers/user/user';
 import { User } from '../../../../../../../models/User';
+import { Validators, FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html',
 })
-export class ProfilePage {
+export class ProfilePage implements OnInit {
   user1: User;
-  constructor(public navCtrl: NavController, public navParams: NavParams, private userService : UserProvider) {
+  private updateProfil: FormGroup;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private formBuilder: FormBuilder, 
+    private userService : UserProvider
+    ) {
   }
 
 
   ngOnInit() {
+   
     console.log('ionViewDidLoad ProfilePage');
     this.user1 = this.userService.getUser();
+    console.log(this.user1);
+    this.initForm();
+  }
+
+  initForm() {
+    this.updateProfil = this.formBuilder.group(
+      {
+        lastName: ["", Validators.required],
+        firstName: ["", Validators.required],
+        phone: ["", Validators.required],
+        email: ["", Validators.compose([Validators.email, Validators.required])],
+        sex: ["", Validators.required],
+        dateOfBirth: ["", Validators.required],
+        password: ["", Validators.required],
+      },
+    );
   }
 
 }
