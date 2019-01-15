616ef6dd0cce466a12094c8cc8264ebcaa32851f
Alexandre GUERIN <alexandre.guerin17@gmail.com>
2019-01-15 17:32:04 +0100

    zoom function working, to polish

--- Front/autostop/src/pages/home/connexion/main/map/map.ts
+++ Front/autostop/src/pages/home/connexion/main/map/map.ts
@@ -112,16 +112,16 @@ export class MapPage {
     this.searchValue = $event
     this.getRouteJson($event).subscribe((data: any) => {
       this.displayRoute(data.routes[0].overview_polyline.points);
-      // this.addMarkerAndCircle();
-      // this.goToSpecificLocation();
+      this.addMarkerAndCircle();
+      this.goToSpecificLocation();
     });
   }
 
   displayRoute(routeJson) {
     this.map.clear().then(() => {
       this.showPoly(routeJson);
-      this.addMarkerAndCircle();
-      this.goToSpecificLocation();
+      // this.addMarkerAndCircle();
+      // this.goToSpecificLocation();
     });
   }
 
@@ -225,17 +225,52 @@ export class MapPage {
   defineZoom(bounds: LatLngBounds){
     console.log(bounds);
 
-    let ecartMax = bounds.northeast.lat - bounds.southwest.lat;
+    let ecartMaxLat = bounds.northeast.lat - bounds.southwest.lat;
+    let ecartMaxLng = bounds.northeast.lng - bounds.southwest.lng;
+    console.log(ecartMaxLng)
+    let ecartMax;
+    if(ecartMaxLat>ecartMaxLng){
+      ecartMax = ecartMaxLat;
+    }
+    else ecartMax = ecartMaxLng;
+
     console.log(ecartMax);
     let answer;
     if(ecartMax<0.01){
       answer = 15;
     }
-    else if(ecartMax<0.02 && ecartMax>=0.01){
-      answer = 5;
+    else if(ecartMaxLat<0.03 && ecartMaxLat>=0.01 || ecartMaxLng<0.05 && ecartMaxLng>=0.01){
+      answer = 14;
+    }
+    else if(ecartMaxLat<0.05 && ecartMaxLat>=0.03 || ecartMaxLng<0.08 && ecartMaxLng>=0.05){
+      answer = 13;
+    }
+    else if(ecartMaxLat<0.07 && ecartMaxLat>=0.05 || ecartMaxLng<0.10 && ecartMaxLng>=0.08){
+      answer = 12;
+    }
+    else if(ecartMaxLat<0.09 && ecartMaxLat>=0.07 || ecartMaxLng<0.13 && ecartMaxLng>=0.10){
+      answer = 11;
+    }
+    else if(ecartMaxLat<0.11 && ecartMaxLat>=0.09 || ecartMaxLng<0.16 && ecartMaxLng>=0.13){
+      answer = 10;
+    }
+    else if(ecartMaxLat<0.13 && ecartMaxLat>=0.11 || ecartMaxLng<0.20 && ecartMaxLng>=0.16){
+      answer = 10;
+    }
+    else if(ecartMaxLat<0.15 && ecartMaxLat>=0.13 || ecartMaxLng<0.24 && ecartMaxLng>=0.20){
+      answer = 9;
+    }
+    else if(ecartMaxLat<0.17 && ecartMaxLat>=0.15 || ecartMaxLng<0.28 && ecartMaxLng>=0.24){
+      answer = 9;
+    }
+    else if(ecartMaxLat<0.19 && ecartMaxLat>=0.17 || ecartMaxLng<0.32 && ecartMaxLng>=0.28){
+      answer = 8;
+    }
+    else if(ecartMaxLat<0.21 && ecartMaxLat>=0.19 || ecartMaxLng<0.36 && ecartMaxLng>=0.32){
+      answer = 8;
     }
     else {
-      answer = 1;
+      answer = 7;
     }
     return answer;
   }
