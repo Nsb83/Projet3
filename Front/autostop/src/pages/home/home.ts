f51b00dfb236175bcc90fd953918ca38f4db915c
Nicolas Barbier <Nicolas-Barbier_student2018@wilder.school>
2019-01-17 22:26:30 +0100

    Connexion automatique if Token is present....LOL

--- Front/autostop/src/pages/home/home.ts
+++ Front/autostop/src/pages/home/home.ts
@@ -1,19 +1,37 @@
 import { MainPage } from "./connexion/main/main";
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NavController } from "ionic-angular";
 import { RegisterPage } from "./register/register";
 import { ConnexionPage } from "./connexion/connexion";
 import { MenuPage } from "./connexion/main/menu/menu";
+import { TokenStorage } from "../../providers/auth/token.storage";
+import { UserProvider } from "../../providers/user/userProvider";
+import { ChoicePage } from "./register/choice/choice";
 
 
 @Component({
   selector: "page-home",
   templateUrl: "home.html"
 })
-export class HomePage {
-  constructor(public navCtrl: NavController) {}
+export class HomePage implements OnInit {
+  constructor(
+    public navCtrl: NavController,
+    public tokenStorage: TokenStorage,
+    public userService: UserProvider
+    ) {}
+
   main = MainPage;
   register = RegisterPage;
   connexion = ConnexionPage;
   menu = MenuPage;
+
+
+ngOnInit() {
+  if(this.tokenStorage.getToken() != null) {
+    this.userService.getUserId();
+    this.navCtrl.push(ChoicePage);
+  }
+
+}
+
 }
