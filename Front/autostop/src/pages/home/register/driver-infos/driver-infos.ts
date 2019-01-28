c39f475947395d0eb91afde335e29eacac724e30
facte <shems.chelgoui@gmail.com>
2019-01-28 10:31:06 +0100

    fixed driver button on choice page and color picker

--- Front/autostop/src/pages/home/register/driver-infos/driver-infos.ts
+++ Front/autostop/src/pages/home/register/driver-infos/driver-infos.ts
@@ -26,7 +26,7 @@ export class DriverInfosPage {
 
   currentFileUpload: File;
   selectedFiles: FileList;
-  driver:Driver;
+  driver:Driver = new Driver();
 
   constructor(public modal: ModalController,
               public navCtrl: NavController,
@@ -43,7 +43,12 @@ export class DriverInfosPage {
 
   ngOnInit() {
     this.initForm();
-    this.driver = this.driverProvider.getDriver();
+    this.driverProvider.getDriver().subscribe((response: any) => {
+      this.driver.setBrand(response.driver.brand);
+      this.driver.setModel(response.driver.model);
+      this.driver.setLicensePlate(response.driver.licensePlate);
+      this.driver.setColor(response.driver.color);
+    });
   }
 
 
