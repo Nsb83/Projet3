9d49ca3077611148bf6f1edb46cd4d4a68198fa1
Alexandre GUERIN <alexandre.guerin17@gmail.com>
2019-01-29 15:28:56 +0100

    map.ts commented

--- Front/autostop/src/pages/home/connexion/main/map/map.ts
+++ Front/autostop/src/pages/home/connexion/main/map/map.ts
@@ -2,7 +2,7 @@ import { UserProvider } from './../../../../../providers/user/userProvider';
 import { RouteProvider } from './../../../../../providers/route/route';
 import { User } from './../../../../../models/User';
 import { RequestModalPage } from './request-modal/request-modal';
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController, AlertController, NavParams, ModalController, Events } from 'ionic-angular';
 import { Trip } from '../../../../../models/Trip';
 import { MessageProvider } from '../../../../../providers/Messages/MessageProvider';
@@ -29,14 +29,10 @@ import { DriverProvider } from '../../../../../providers/driver/driverProvider';
 import { MatchingUserDetails } from '../../../../../models/MatchingUserDetails';
 import { Observable } from 'rxjs';
 import { PedestrianProvider } from '../../../../../providers/Pedestrian/PedestrianProvider';
-import { ObserveOnOperator } from 'rxjs/operators/observeOn';
 import { takeWhile } from 'rxjs/operators';
 import { ResponseModalPage } from './request-modal/response-modal/response-modal';
 import { MatchProvider } from '../../../../../providers/match/matchProvider';
 import { MatchingEntity } from '../../../../../models/MatchingEntity';
-
-
-// @IonicPage()
 @Component({
   selector: "page-map",
   templateUrl: "map.html"
@@ -47,7 +43,6 @@ export class MapPage {
   inputSearch: String;
   userPosition: MyLocation;
   searchValue: any;
-  routeJson : any;
   polyline: Polyline;
   markerDestination : Marker;
   iconPath: String;
@@ -61,10 +56,7 @@ export class MapPage {
   pollingPedestrians: any;
   requestingMatchingEntities: MatchingEntity[];
   modalShowed: boolean = false;
-
   option: MyLocationOptions = {
-    // true use GPS as much as possible (lot battery)
-    // false network location (save battery)
     enableHighAccuracy: true
   };
 
@@ -112,25 +104,40 @@ export class MapPage {
               });
             }
 
+
+  /**
+   * Getting the user at the initialisation of the component.
+   */
   ngOnInit(){
     this.userProvider.getUser().subscribe(response => {
       this.user = response;
     });
   }
 
-  ngOnDestroy() {
-    this.pollingPedestrians.unsubscribe();
-  }
-
-  // Load map only after view is initialized
+  /**
+   * Loading the map only after the view is initialised.
+   */
   ngAfterViewInit() {
     this.loadMap();
   }
 
+  /**
+   * Setting the geolocation icon
+   */
   ngAfterViewChecked(){
     this.setIconUser();
   }
 
+  /**
+   * Unsubscribing to the stream of data from the matching request.
+   */
+  ngOnDestroy() {
+    this.pollingPedestrians.unsubscribe();
+  }
+
+  /**
+   * Setting the path of the geolocation icon from the vehiculed status of the user we catched at the initialisation.
+   */
   setIconUser(){
     if(this.user.isVehiculed()) {
       this.iconPath = "../assets/icon/driver.png";
@@ -138,13 +145,15 @@ export class MapPage {
     else this.iconPath = "../assets/icon/thumb.png";
   }
 
+  /**
+   * Creation of the map. It is centered on the geolocation of the user. The marker and circle are called here if needed.
+   */
   loadMap() {
     Environment.setEnv({
       API_KEY_FOR_BROWSER_RELEASE: "AIzaSyBhqCcaN5OfApXOWr_1b2VkIBQqIwPQK44",
       API_KEY_FOR_BROWSER_DEBUG: "AIzaSyBhqCcaN5OfApXOWr_1b2VkIBQqIwPQK44"
     });
 
-    // Création de la carte avec géolocalisation
     LocationService.getMyLocation(this.option).then((location: MyLocation) => {
       this.userPosition = location;
       this.map = GoogleMaps.create("map");
@@ -156,11 +165,12 @@ export class MapPage {
     });
   }
 
+  /**
+   * Creation of a marker on the user position. A circle is added if he is a pedestrian. It have a radius value linked to the search radius attribute of the pedestrian.
+   *
+   * The marker options set the icon path depending on the vehiculed attribute.
+   */
   addMarkerAndCircle() {
-
-      // MARKER
-      // Création d'un marqueur et son ajout à map avec la géoloc
-      // Possibilité de passer un objet Options en param
         let markerOptions: MarkerOptions;
         if(this.user.isVehiculed()) {
           markerOptions = {
@@ -189,23 +199,27 @@ export class MapPage {
       let markerGeoloc: Marker = this.map.addMarkerSync(markerOptions);
 
       if(!this.user.isVehiculed()) {
-        // CERCLE POUR PIÉTON
-        // création d'un objet avec lat et lng à partir de la géoloc
         let center = this.userPosition.latLng;
         let pedestrian = this.user.getPedestrian();
 
-        // let radius = this.user.getPedestrian().getSearchRadius();
-        // création du cercle avec comme centre la géoloc
         let circle: Circle = this.map.addCircleSync({
           center: center,
           radius: pedestrian.searchRadius,
           strokeColor: "#258c3d",
-          // strokeWidth: 30, A QUOI CA SERT???
           fillColor: "rgba(239, 244, 225, 0.45)"
         });
       }
     }
 
+  /**
+   * Catch the search value from the search bar then :
+   *
+   * - Get the directions of the trip
+   * - A new trip is created from the response, ready to be sent
+   * - The drawing of the trip is displayed from the overview polyline
+   * - An end trip marker is added
+   * @param $event
+   */
   receiveMessage($event) {
     this.searchValue = $event;
     this.getRouteJson($event).subscribe(
@@ -218,14 +232,31 @@ export class MapPage {
                               );
           this.displayRoute(data.routes[0].overview_polyline.points);
           this.addMarkerAndCircle();
-          this.goToSpecificLocation();
+          this.showDestinationMarker();
         } else {
           this.messageProvider.myAlertMethod("Désolé", "Nous n'avons pas trouvé votre adresse de destination...", false);
         }
       });
   }
 
-  displayRoute(routeJson) {
+  /**
+   * Call to the google maps direction api.
+   *
+   * Use the user geolocation Lat & Lng and the search value from the search bar.
+   * @param searchValue
+   * @returns Json of directions to be used to create a trip
+   */
+  getRouteJson(searchValue) {
+    return this.routeProvider.getRoute(this.userPosition.latLng, searchValue);
+  }
+
+  /**
+   * Call the display of the user trip on the map.
+   *
+   * If one or multiples trips are displayed, they are removed with the markers.
+   * @param overview_polyline Encoded array of lat lng values defining a trip
+   */
+  displayRoute(overview_polyline) {
     if (this.polyline !== undefined) {
       this.polyline.remove();
       this.markerDestination.remove();
@@ -235,36 +266,28 @@ export class MapPage {
         this.arrayPolyMatched[i].remove();
       }
     }
-    this.showPoly(routeJson, '#258c3d');
+    this.showPoly(overview_polyline, '#258c3d');
   }
 
-  getRouteJson(searchValue) {
-      return this.routeProvider.getRoute(this.userPosition.latLng, searchValue);
-  }
-
-  ///////// Polylines ////////////////////
+  /**
+   * Pass the overview polyline in a decoder. An array of Lat Lng is created, representing the trip.
+   *
+   * This array is then used to display the trip on the map.
+   *
+   * The camera is moved to fit the trip on the screen.
+   * @param polyRoute overview polyline from google maps direction API
+   * @param polyColor color choice for the drawing
+   */
   showPoly(polyRoute, polyColor) {
-      const decodePolyline = require('decode-google-map-polyline');
-      this.arrayPoly = decodePolyline(polyRoute);
+    const decodePolyline = require('decode-google-map-polyline');
+    this.arrayPoly = decodePolyline(polyRoute);
 
     this.polyline = this.map.addPolylineSync({
       points: this.arrayPoly,
       color: polyColor,
       width: 5,
       geodesic: true,
-      // clickable: true
     })
-
-    // this.polyline.on(GoogleMapsEvent.POLYLINE_CLICK).subscribe((params: any) => {
-    //   let position: LatLng = <LatLng>params[0];
-    //   let markerPoly: Marker = this.map.addMarkerSync({
-    //     position: position,
-    //     title: position.toUrlValue(),
-    //     disableAutoPan: true
-    //   });
-
-    //   markerPoly.showInfoWindow();
-    // });
 
     this.map.moveCamera({
       'target': this.arrayPoly
@@ -272,6 +295,39 @@ export class MapPage {
 
   }
 
+  /**
+   * Put a marker on the destination of the trip with a specific icon.
+   */
+  showDestinationMarker() {
+    let options: GeocoderRequest = {
+    address: this.searchValue
+    };
+
+    Geocoder.geocode(options)
+    .then((results: GeocoderResult[]) => {
+      this.destinationILatLng = results[0].position;
+      this.markerDestination = this.map.addMarkerSync({
+      'position': results[0].position,
+      'title': JSON.stringify(results[0].extra.lines),
+      icon: {url: "../assets/icon/endTripPin.png",
+                size: {
+                  width: 50,
+                  height: 30
+                }
+              }
+      })
+    });
+  }
+
+  /**
+   * Function called on click to validate the trip.
+   *
+   * If the user is pedestrian, his trip is updated and he received the matched users to be then displayed with showMatchedUsersPoly.
+   *
+   * If the user is a driver, he starts listening to the matchings entities on the back.
+   * If a pedestrian requested a pick up, his matching entity will be updated.
+   * It will then display a modal to accept or decline the pick up and stop the polling.
+   */
   sendTrip() {
     this.tripProvider.updateTrip(this.validatedTrip).subscribe(() => {
       if(this.user.isVehiculed()) {
@@ -285,7 +341,9 @@ export class MapPage {
           ]
         });
         alert.present();
+
         this.modalShowed = false;
+
         this.pollingPedestrians = Observable.interval(1000)
           .pipe(takeWhile(() => !this.modalShowed))
           .switchMap(() => this.matchProvider.queryPedestrian())
@@ -324,37 +382,21 @@ export class MapPage {
     })
   }
 
-
-  goToSpecificLocation() {
-    let options: GeocoderRequest = {
-    address: this.searchValue
-    };
-
-    Geocoder.geocode(options)
-    .then((results: GeocoderResult[]) => {
-      this.destinationILatLng = results[0].position;
-      this.markerDestination = this.map.addMarkerSync({
-      'position': results[0].position,
-      'title': JSON.stringify(results[0].extra.lines),
-      icon: {url: "../assets/icon/endTripPin.png",
-                size: {
-                  width: 50,
-                  height: 30
-                }
-              }
-      })
-    });
-  }
-
-  // Show modal for matching request
+  /**
+   * Display the response modal where the driver will accept or decline the trip and the pedestrian can cancel it.
+   * @param matchUser
+   * @param matchingEntity
+   */
   showMatchModal(matchUser, matchingEntity) {
-    this.navCtrl.push(ResponseModalPage, {
-                                          matchableUser : matchUser,
-                                          matchingEntity : matchingEntity
-    });
+    this.navCtrl.push(ResponseModalPage, { matchableUser : matchUser, matchingEntity : matchingEntity });
   }
 
-
+  /**
+   * First remove all the drawed polylines if there are some.
+   *
+   * Then call the matching function on the back.
+   * It will return an array of matching users that will passed to showPolyMatch ti be displayed.
+   */
   showMatchedUsersPoly(){
     if (this.arrayPolyMatched !== null){
       for(let i=0; i <= this.arrayPolyMatched.length -1; i++){
@@ -374,6 +416,14 @@ export class MapPage {
     });
   }
 
+  /**
+   * Decode the overview polyline of the trip and display it. Open the match modal if clicked.
+   *
+   * Camera is moved to fit the trip in the screen.
+   * @param polyRoute overview polyline of the trip
+   * @param polyColor
+   * @param driverInfos matching user data to be passed in the match modal
+   */
   showPolyMatch(polyRoute, polyColor, driverInfos) {
     const decodePolyline = require('decode-google-map-polyline');
     this.arrayPoly = decodePolyline(polyRoute);
