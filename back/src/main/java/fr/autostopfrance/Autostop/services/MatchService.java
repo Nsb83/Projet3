e95e2eb54d0a1fd71c85a5bb3c93d8d6dcd3d7e1
JN Bourrat <jn.brrt@yahoo.fr>
2019-01-29 11:54:39 +0100

    Javadoc finished in matchController (back)

--- back/src/main/java/fr/autostopfrance/Autostop/services/MatchService.java
+++ back/src/main/java/fr/autostopfrance/Autostop/services/MatchService.java
@@ -38,6 +38,18 @@ public class MatchService {
 
 	private String apiKey = "AIzaSyBhqCcaN5OfApXOWr_1b2VkIBQqIwPQK44";
 	
+	/**
+	 * Returns an ArrayList of AlgoObject that can then be used to locate the nearest driver step. 
+	 * <p>
+	 * The argument itinerary is a List of {@link LatLng} from the driver's route.
+	 * The argument pedestrianLatLng is the location of the pedestrian we want the driver's itinerary to be compared to. 
+	 *
+	 * @param  itinerary  an absolute URL giving the base location of the image
+	 * @param  pedestrianLatLng the location of the pedestrian
+	 * @return      an array of AlgoObject
+	 * @see         AlgoObject
+	 * @see 		LatLng
+	 */	
 	public ArrayList<AlgoObject> getAlgoTable(List<LatLng> itinerary, LatLng pedestrianLatLng) {
 		
 		ArrayList<AlgoObject> response = new ArrayList<AlgoObject>();
@@ -55,6 +67,18 @@ public class MatchService {
 		
 	}
 
+	/**
+	 * Returns a LatLng which represents the location of the nearest step between a driver's itinerary and a pedestrian position. 
+	 * <p>
+	 * The argument itinerary is a List of {@link LatLng} from the driver's route.
+	 * The argument pedestrianLatLng is the location of the pedestrian we want the driver's itinerary to be compared to. 
+	 *
+	 * @param  algoTable  an ArrayList of AlgoObect
+	 * @param  pedestrianLocation
+	 * @return      a LatLng representing the nearest position from the pedestrian location to the driver's route
+	 * @see         AlgoObject
+	 * @see 		LatLng
+	 */	
 	public LatLng getNearestDriverStep(ArrayList<AlgoObject> algoTable, LatLng pedestrianLocation) {
 		
 		int index = 0;
@@ -70,6 +94,16 @@ public class MatchService {
 		return algoTable.get(indexMin).getCoordinate();
 	}
 	
+	/**
+	 * Calculate the distance in meters between the two {@link LatLng} arguments and returns it. 
+	 * <p> The distance is calculated using Google {@link DistanceMatrixApi} API, in WALKING MODE and in METERS.
+	 *
+	 * @param  		
+	 * @return   	the distance in meters between the two arguments
+	 * @see 		DistanceMatrixApi
+	 * @see			DistanceMatrix
+	 * @see 		LatLng
+	 */	
 	public long calculateDistance(LatLng pedestrianLatLng, LatLng driverNearestStep) {
 		
 		GeoApiContext context = new GeoApiContext.Builder()
@@ -100,6 +134,13 @@ public class MatchService {
 		return distance;
 	}
 	
+	/**
+	 * Returns a boolean depending on the distance of the nearest driver's step
+	 * and the searchRadius specified by the pedestrian.
+	 *
+	 * @param  		
+	 * @return   	true if the users are matchable, false if they are not
+	 */	
 	public boolean areUsersMatchable(int pedestrianSearchRadius, long distance) {
 		if (distance >= 0 && distance <= pedestrianSearchRadius) {
 			return true;
@@ -108,7 +149,14 @@ public class MatchService {
 		}
 	}
 	
-
+	/**
+	 * Create a new {@link MatchingEntity} in the database, using {@link MatchingDAO}, and returns it.
+	 *
+	 * @param  		
+	 * @return   	the matchingEntity just added to the database
+	 * @see			MatchingEntity
+	 * @see 		MatchingDAO
+	 */	
     public MatchingEntity registerMatchingDriver (String pedestrianPublicId, MatchingEntity matchingEntity) {
         System.out.println("Updating MatchingEntity" + matchingEntity);
 
@@ -121,6 +169,13 @@ public class MatchService {
         return _matchingEntity;
     }
     
+    /**
+	 * Returns the {@link MatchingEntity}'s attribute "accepted" (true or false) retrieved from the database with the id of the matchingEntity.
+	 *
+	 * @param  		
+	 * @see			MatchingEntity
+	 * @see 		MatchingDAO
+	 */	
     public boolean getMatchingEntityStatus(Long id) {
     	Optional<MatchingEntity> matchingEntity = matchingDAO.findById(id);
     	
@@ -128,7 +183,12 @@ public class MatchService {
     	
     	return _matchingEntity.isAccepted();
     }
-
+    
+    /**
+	 * @param  	matchingEntity	the new MatchingEntity	
+	 * @see			MatchingEntity
+	 * @see 		MatchingDAO
+	 */	
 	public MatchingEntity updateMatchingEntity(Long id, MatchingEntity matchingEntity) {
 		
 		Optional<MatchingEntity> optionalMatchingEntity = matchingDAO.findById(id);
@@ -140,9 +200,17 @@ public class MatchService {
 		return _matchingEntity;
 	}
 	
+	/**
+	 * Returns a List of {@link MatchingEntity} retrieved from the database with the publicId of a {@link Driver}.
+	 * <p>
+	 * This method is usually called from the front-end using a polling strategy (every X seconds).
+	 *
+	 * @param  		
+	 * @see			MatchingEntity
+	 * @see 		MatchingDAO
+	 */	
 	public LinkedList<MatchingEntity> checkPedestrianRequest (String driverPublicId) {
         LinkedList<MatchingEntity> matchingEntities = matchingDAO.findByDriverPublicId(driverPublicId);
-        System.out.println("Matcher " + driverPublicId);
         if (matchingEntities == null)
             throw new UsernameNotFoundException("No travel asked for " + driverPublicId + " yet!");
         
@@ -150,6 +218,13 @@ public class MatchService {
 
     }
 
+	/**
+	 * Deletes a {@link MatchingEntity} from the database.
+	 *
+	 * @param  		
+	 * @see			MatchingEntity
+	 * @see 		MatchingDAO
+	 */	
 	public ResponseEntity<String> deleteMatchingEntity(Long id) {
 		Optional<MatchingEntity> matchingEntity = matchingDAO.findById(id);
         matchingDAO.delete(matchingEntity.get());
